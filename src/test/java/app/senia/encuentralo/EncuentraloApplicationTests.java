811b9836e0642f220340de829ac4d92e2e981bf8
Marcel Garcia <marcelgb.dev@gmail.com>
2026-05-13 14:57:19 +0200

    SpringBoot application initialized

---
+++ src/test/java/app/senia/encuentralo/EncuentraloApplicationTests.java
@@ -0,0 +1,13 @@
+package app.senia.encuentralo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncuentraloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
