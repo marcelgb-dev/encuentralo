47f465cd32e8d48f136afbffaa4eb9e23e71ec32
Marcel <marcelgb.dev@gmail.com>
2026-05-14 20:28:55 +0200

    - Import añadido para BusinessDTO

--- src/main/java/app/senia/encuentralo/dto/yelp/BusinessDTO.java
+++ src/main/java/app/senia/encuentralo/dto/yelp/BusinessDTO.java
@@ -3,6 +3,8 @@ package app.senia.encuentralo.dto.yelp;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.List;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
 record BusinessDTO(
         String name,
