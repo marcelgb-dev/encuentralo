7bf798d7d0e4a3a589b4b91653f217286bc3379e
Marcel <marcelgb.dev@gmail.com>
2026-05-28 16:09:51 +0200

    Arreglados headers de csv y empezado trabajo en etiquetas

--- src/main/java/app/senia/encuentralo/service/EtiquetaService.java
+++ src/main/java/app/senia/encuentralo/service/EtiquetaService.java
@@ -1,6 +1,7 @@
 package app.senia.encuentralo.service;
 
 import app.senia.encuentralo.model.Etiqueta;
+import app.senia.encuentralo.repository.EtiquetaRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -8,9 +9,17 @@ import java.util.List;
 @Service
 public class EtiquetaService {
 
+    private final EtiquetaRepository etiquetaRepo;
+
+    public EtiquetaService(EtiquetaRepository etiquetaRepo) {
+        this.etiquetaRepo = etiquetaRepo;
+    }
+
+
     public List<Etiqueta> obtenerEtiquetasUsuario(Integer usuarioId) {
         // Lógica para obtener todas las etiquetas de un usuario
-        return null;
+        List<Etiqueta> etiquetas = etiquetaRepo.findByUsuarioId(usuarioId);
+        return etiquetas;
     }
 
     public List<Etiqueta> obtenerEtiquetasResultado(Integer resultadoId) {
